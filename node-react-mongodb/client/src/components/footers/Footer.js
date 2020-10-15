19121ba0a5101c96509266f27d91c42e08276dbe
Jngy <s3165643@student.rmit.edu.au>
2020-10-16 09:49:48 +1100

    Updated Footer.js: Added Proprietary Image Disclaimer

--- node-react-mongodb/client/src/components/footers/Footer.js
+++ node-react-mongodb/client/src/components/footers/Footer.js
@@ -6,12 +6,17 @@ function Footer() {
     return (
         <footer>
             <div className="container">
-                <h1 className="heading">
-                    MELLOCAL
-                </h1>
-                <p className="copyright">
-                    &copy; 2020
-                </p>
+                <div className="left_footer">
+                    <h1 className="heading">
+                        MELLOCAL
+                    </h1>
+                    <p className="copyright">
+                        &copy; 2020
+                    </p>
+                </div>
+                <div className="right_footer">
+                    <p>Proprietary images used for Educational purposes only</p>
+                </div>
             </div>
         </footer>
     )
