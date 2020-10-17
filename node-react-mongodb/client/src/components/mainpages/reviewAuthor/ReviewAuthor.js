125c34bb4caeb59102d8be1e254cf1627a3b0e70
Jngy <s3165643@student.rmit.edu.au>
2020-10-17 17:02:47 +1100

    Added node-react-mongodb/client/*/ReviewAuthor

---
+++ node-react-mongodb/client/src/components/mainpages/reviewAuthor/ReviewAuthor.js
@@ -0,0 +1,29 @@
+import React, { useContext, useState, useParams } from 'react'
+import { Link } from 'react-router-dom'
+import { GlobalState } from '../../../GlobalState'
+
+
+// Import Bootstrap React Components
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
+
+
+
+export default function ReviewAuthor() {
+
+
+
+
+
+         return (
+        <div>
+        </div>
+    )
+
+    
+
+
+
+}
